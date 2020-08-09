0f405c16c73fc0e4293eece74ea65d8ef3ac93d2
skaraninder <39990752+skaraninder@users.noreply.github.com>
2020-08-09 20:56:27 +1000

    updated

--- Python Code/Parent.py
+++ Python Code/Parent.py
@@ -2,6 +2,7 @@ import Preprocessor
 import pandas as pd
 from algoSelection import chi_square,select_k_best,correlation,classification_compare
 import HyperTuning
+from HyperTuning import ensembler,Hyper_Tuning
 '''
 frame = pd.concat([pd.read_csv('/content/drive/My Drive/Colab Notebooks/TweetsCOV19.tsv',sep = '\t',skiprows = list(range(4467891,8935782)),usecols=range(11),header=None),pd.read_csv('/content/drive/My Drive/Colab Notebooks/TweetsCOV19.tsv',sep = '\t',skiprows = list(range(4467891)),usecols=range(11),header=None)])
 frame.columns = ['Tweet Id', 'Username', 'Timestamp', 'Followers', 'Friends', 'Retweets', 'Favorites', 'Entities', 'Sentiment', 'Mentions', 'Hashtags']
@@ -20,11 +21,11 @@ train_set.drop(['Tweet Id'],axis=1,inplace=True)
 test_set.drop(['Tweet Id'],axis=1,inplace=True)
 
 #feature selection
-chi_square(train_set)
-correlation(train_set)
+# chi_square(train_set)
+# correlation(train_set)
 
 #algo selection
 # classification_compare(train_set,test_set)
 
 #hyper-tuning and prediction
-HyperTuning.Hyper_Tuning(train_set, test_set)
+Hyper_Tuning(train_set, test_set)
